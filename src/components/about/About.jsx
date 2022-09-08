824171ab2557fc4eb8db23c6fe41ed6b8600be2a
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2022-09-09 00:05:11 +0300

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,9 +3,10 @@ import { Typewriter} from 'react-simple-typewriter';
 import Portrait from './20220908_051312.jpg';
 
 const About = () => {
+
   return (
       <div className=' w-[90%] h-full' id='about'>
-        <section className=' w-full grid grid-cols-1 md:grid-cols-2 gap-2 my-36 '>
+        <section className=' w-full grid grid-cols-1 md:grid-cols-2 gap-2 my-36'>
           <div className=' w-full h-320 flex items-center'>
             <div className=' relative w-275 h-340 bg-gray-100 rounded-md'>
               <img className=' w-full h-full absolute -right-4 top-4 object-cover rounded-lg shadow-lg ' src={Portrait} alt='' />
@@ -13,10 +14,21 @@ const About = () => {
           </div>
 
           <div className='w-full h-320 flex flex-col'>
-          <h2 className=' text-2xl text-682AE9 '>
+
+          <h2 className=' text-2xl text-682AE9 mt-10 '>
           I'm a 
-          <span className=' text-red-400'> <Typewriter cursor loop cursorStyle='.' typeSpeed={230} deleteSpeed={50} delaySpeed={1000} words={['FrontEndDeveloper( )']} /></span>
+          <span className=' text-red-400'> <Typewriter
+            words={['FrontEndDeveloper( )']}
+            loop={1}
+            cursor
+            cursorStyle='.'
+            typeSpeed={200}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            onLoopDone={() => console.log(`Done after 5 loops!`)}
+          /></span>
           </h2>
+
           <p className='text-lg text-textBase mt-5'>
           Programmers projects in the free software development project began in the free software development project, creating access to a lot of information
           Programmers projects in the free software development project began in the free software development project, creating access to a lot of information
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,25 @@ import {IoMenu} from 'react-icons/io5';
 const Navbar = () => {
 
     const [show, setShow] = useState(false);
+    const [showCode, setShowCode] = useState(false)
+    const [inputValue, setInputValue] = useState('')
+    // const [sections, setSections] = useState(['Home()','About()'])
 
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // sections.map((section) => {})
+        if(inputValue === "About()"){
+            window.location = '#about'
+        }else if(inputValue === "Home()"){
+            window.location = '#home'
+        }
+        setShowCode(false)
+    }
   return (
     <nav className=' w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center '>
         <div className=' w-full md:w-880  p-4 rounded-2xl flex justify-between items-center '>
@@ -16,7 +34,18 @@ const Navbar = () => {
             <a href='#projects' className=' text-base text-dbdbdb font-medium hover:text-682AE9 cursor-pointer duration-100 ease-in-out'>.projects( )</a>
             <a href='#skills' className=' text-base text-dbdbdb font-medium hover:text-682AE9 cursor-pointer duration-100 ease-in-out'>.skils( )</a>
             <a href='#contact' className=' text-base text-dbdbdb font-medium hover:text-682AE9 cursor-pointer duration-100 ease-in-out'>.contact( )</a>
+
+            <span  onClick={() => {setShowCode(!showCode)}} className=' text-base text-dbdbdb font-medium hover:text-682AE9 cursor-pointer duration-100 ease-in-out'>.test( )</span>
+            
             </div>
+
+            {showCode && (
+               <div className=' w-275   fixed top-24 right-5 flex flex-col items-center justify-evenly '>
+               <input onChange={handleChange} className='border border-purple-600  w-full h-full p-4 rounded-lg bg-0B0F15 text-amber-500 font-mono ' type='text' placeholder='Write the title of the page on the function call method ..' />
+                <button onClick={handleSubmit} className=' w-full bg-682AE9  text-blue-50 outline-hidden p-2 rounded-lg mt-5'>Go</button>
+               </div>
+            )}
+            
 
              {/* menu */}
             <div className='block md:hidden cursor-pointer ml:auto'>

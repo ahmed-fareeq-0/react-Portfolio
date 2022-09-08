5251587a17ac8aec57264f673e859e39886bc69e
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2022-09-08 05:28:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar';
+import Spline3D from './components/spline/Spline3D.jsx'
 
 function App() {
   return (
     <div className="flex w-screen  min-h-screen flex-col justify-center items-center relative bg-0B0F15  " id='home'>
-    `<Navbar />
+      <Navbar />
+      <Spline3D />
     </div>
   );
 }
---
+++ src/components/spline/Spline3D.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Spline from '@splinetool/react-spline';
+
+const Spline3D = () => {
+  return (
+    <div className=' relative w-screen h-screen'>
+        <Spline scene="https://prod.spline.design/JbE1xEFzROZgXYZW/scene.splinecode" />
+    </div>
+  );
+}
+
+export default Spline3D;
